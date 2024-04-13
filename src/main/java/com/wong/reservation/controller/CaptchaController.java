0b27b3af8920dad2870ff68576c70809164794ce
Wongbuer <wongbuer@outlook.com>
2024-04-13 21:56:28 +0800

    style(captcha模块): 生成javadoc

--- src/main/java/com/wong/reservation/controller/CaptchaController.java
+++ src/main/java/com/wong/reservation/controller/CaptchaController.java
@@ -23,11 +23,26 @@ public class CaptchaController {
     @Resource
     private CaptchaService captchaService;
 
+    /**
+     * 生成验证码
+     *
+     * @param request HttpServletRequest
+     * @param response HttpServletResponse
+     * @return Result<Map<String, Object>> 图片验证码和其UUID
+     * @throws IOException IO异常
+     */
     @RequestMapping("/generate")
     public Result<Map<String, Object>> captcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
         return captchaService.generateCaptcha(request, response);
     }
 
+    /**
+     * 验证验证码
+     *
+     * @param request HttpServletRequest
+     * @param captchaDTO 验证码
+     * @return boolean 验证结果
+     */
     @RequestMapping("/verify")
     public boolean verify(HttpServletRequest request, CaptchaDTO captchaDTO) {
         return captchaService.verifyCaptcha(request, captchaDTO);

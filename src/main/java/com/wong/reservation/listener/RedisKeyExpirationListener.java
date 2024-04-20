096f1db6624bbb4e2264ede64d9c7f82cb9e8e97
Wongbuer <wongbuer@outlook.com>
2024-04-20 14:14:51 +0800

    fix(lock模块, order模块): 修复创建订单上锁逻辑, 优化可重入锁定时任务方式的清除逻辑
    1. 将创建订单的上锁key由orderId改为employeeId, 避免同一个员工的相同预约时间的并发创建订单导致的时间冲突问题
    2. 将可重入锁内部的key逻辑由id扩大到\w+:\d+的id, 前面的\w+用于区分key为employeeId还是orderId, 以便计划任务清除指定锁
    3. 优化properties中的订单锁配置的层级关系
    4. 修复redisListener的相应上锁逻辑

--- src/main/java/com/wong/reservation/listener/RedisKeyExpirationListener.java
+++ src/main/java/com/wong/reservation/listener/RedisKeyExpirationListener.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.wong.reservation.constant.OrderStatusConstant;
 import com.wong.reservation.domain.entity.Order;
 import com.wong.reservation.service.OrderService;
+import com.wong.reservation.utils.lock.OrderLock;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
@@ -12,8 +13,7 @@ import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
-import static com.wong.reservation.constant.SystemConstant.ORDER_ACCEPTED_PREFIX;
-import static com.wong.reservation.constant.SystemConstant.ORDER_CREATED_PREFIX;
+import static com.wong.reservation.constant.SystemConstant.*;
 
 /**
  * @author Wongbuer
@@ -24,6 +24,8 @@ import static com.wong.reservation.constant.SystemConstant.ORDER_CREATED_PREFIX;
 public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
     @Resource
     private OrderService orderService;
+    @Resource
+    private OrderLock orderLock;
 
     public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
         super(listenerContainer);
@@ -35,41 +37,56 @@ public class RedisKeyExpirationListener extends KeyExpirationEventMessageListene
         log.info("redis key: {} expired", redisKey);
         // 判断是否为过期未接受/过期未付款
         String orderId = null;
+        String type = null;
         if (redisKey.startsWith(ORDER_CREATED_PREFIX)) {
+            type = ORDER_LOCK_ACCEPT_PREFIX;
             // 获取value
             orderId = redisKey.substring(ORDER_CREATED_PREFIX.length());
         } else if (redisKey.startsWith(ORDER_ACCEPTED_PREFIX)) {
+            type = ORDER_LOCK_PAY_PREFIX;
             // 获取value
             orderId = redisKey.substring(ORDER_ACCEPTED_PREFIX.length());
         } else {
             return;
         }
         // 获取订单
-        Order order = orderService.getById(orderId);
-        if (ObjectUtils.isEmpty(order)) {
-            throw new RuntimeException("订单不存在");
-        }
+        String lockKey = type + orderId;
+        boolean isLocked = orderLock.acquireLock(lockKey, 3000);
+        if (isLocked) {
+            try {
+                Order order = orderService.getById(orderId);
+                if (ObjectUtils.isEmpty(order)) {
+                    throw new RuntimeException("订单不存在");
+                }
 
-        LambdaUpdateWrapper<Order> wrapper = new LambdaUpdateWrapper<>();
-        wrapper.eq(Order::getId, orderId);
+                LambdaUpdateWrapper<Order> wrapper = new LambdaUpdateWrapper<>();
+                wrapper.eq(Order::getId, orderId);
 
-        // 判断是否为CREATED
-        if (order.getStatus() == OrderStatusConstant.CREATED) {
-            wrapper
-                    // 再次确认订单状态正确
-                    .eq(Order::getStatus, OrderStatusConstant.CREATED)
-                    // 修改订单状态
-                    .set(Order::getStatus, OrderStatusConstant.TIMEOUT_CREATED);
-            updateOrderStatus(wrapper);
-        } else if (order.getStatus() == OrderStatusConstant.ACCEPTED) {
-            wrapper
-                    // 再次确认订单状态正确
-                    .eq(Order::getStatus, OrderStatusConstant.ACCEPTED)
-                    // 修改订单状态
-                    .set(Order::getStatus, OrderStatusConstant.TIMEOUT_PAYMENT);
-            updateOrderStatus(wrapper);
+                // 判断是否为CREATED
+                if (order.getStatus() == OrderStatusConstant.CREATED) {
+                    wrapper
+                            // 再次确认订单状态正确
+                            .eq(Order::getStatus, OrderStatusConstant.CREATED)
+                            // 修改订单状态
+                            .set(Order::getStatus, OrderStatusConstant.TIMEOUT_CREATED);
+                    updateOrderStatus(wrapper);
+                } else if (order.getStatus() == OrderStatusConstant.ACCEPTED) {
+                    wrapper
+                            // 再次确认订单状态正确
+                            .eq(Order::getStatus, OrderStatusConstant.ACCEPTED)
+                            // 修改订单状态
+                            .set(Order::getStatus, OrderStatusConstant.TIMEOUT_PAYMENT);
+                    updateOrderStatus(wrapper);
+                } else {
+                    log.info("订单状态已变化, 已经为: {}", OrderStatusConstant.getStatusName(order.getStatus()));
+                }
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
+            } finally {
+                orderLock.releaseLock(lockKey);
+            }
         } else {
-            log.info("订单状态已变化, 已经为: {}", OrderStatusConstant.getStatusName(order.getStatus()));
+            log.info("锁竞争失败——订单状态应该已经发生变化");
         }
     }
 

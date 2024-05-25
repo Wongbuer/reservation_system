3c76944e4b7c2ffbf18f55acb04a6f75cd1d2374
Wongbuer <wongbuer@outlook.com>
2024-05-25 18:58:49 +0800

    feat(doc): 新增系统功能文档

---
+++ docs/sql/reservation_system.sql
@@ -0,0 +1,200 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80300
+ Source Host           : localhost:3306
+ Source Schema         : reservation_system
+
+ Target Server Type    : MySQL
+ Target Server Version : 80300
+ File Encoding         : 65001
+
+ Date: 25/05/2024 18:54:55
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for address
+-- ----------------------------
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '地址表主键',
+  `user_id` bigint NOT NULL COMMENT '用户id',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '\"收货人\"名称',
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '\"收货人\"电话',
+  `province` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址省份',
+  `city` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址城市',
+  `district` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址区县',
+  `full_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '详细地址',
+  `postal_code` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮政编码',
+  `created_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for chat_log
+-- ----------------------------
+DROP TABLE IF EXISTS `chat_log`;
+CREATE TABLE `chat_log`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '聊天信息主键',
+  `sender_id` bigint NOT NULL COMMENT '发送方id(与接收方id组成unique键)',
+  `receiver_id` bigint NOT NULL COMMENT '接收方id',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发送内容',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '已读状态(0: 未读, 1: 已读)',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 40 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for employee
+-- ----------------------------
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '员工表id',
+  `user_id` bigint NOT NULL COMMENT '员工对应用户id',
+  `score` tinyint(1) NULL DEFAULT NULL COMMENT '员工评分',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1780582625400328196 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for employee_service
+-- ----------------------------
+DROP TABLE IF EXISTS `employee_service`;
+CREATE TABLE `employee_service`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '员工服务id',
+  `employee_id` bigint NOT NULL COMMENT '员工id',
+  `service_id` bigint NOT NULL COMMENT '服务id',
+  `rate` int NULL DEFAULT NULL COMMENT '服务评分',
+  `price` decimal(10, 2) NOT NULL COMMENT '员工服务单价',
+  `discount` decimal(10, 2) NULL DEFAULT 0.00 COMMENT '服务折扣',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '员工服务描述',
+  `time_unit` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'hour' COMMENT '员工服务计价单位',
+  `detailed_pictures` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '详细介绍图片',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for evaluation
+-- ----------------------------
+DROP TABLE IF EXISTS `evaluation`;
+CREATE TABLE `evaluation`  (
+  `id` bigint NOT NULL COMMENT '用户评价表主键',
+  `order_id` bigint NOT NULL COMMENT '订单id',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '评价内容',
+  `score` int NULL DEFAULT NULL COMMENT '订单分数(5分制)',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单表主键',
+  `user_id` bigint NOT NULL COMMENT '用户id',
+  `employee_id` bigint NULL DEFAULT NULL COMMENT '员工id',
+  `service_id` bigint NOT NULL COMMENT '订单对应服务id',
+  `reservation_time` datetime NOT NULL COMMENT '订单预约时间',
+  `close_time` datetime NULL DEFAULT NULL COMMENT '订单关闭时间',
+  `end_time` datetime NULL DEFAULT NULL COMMENT '订单完成时间',
+  `payment_time` datetime NULL DEFAULT NULL COMMENT '订单支付时间',
+  `accepted_time` datetime NULL DEFAULT NULL COMMENT '订单接受时间',
+  `address_id` bigint NULL DEFAULT NULL COMMENT '地址id',
+  `payment` decimal(10, 2) NULL DEFAULT NULL COMMENT '实付金额',
+  `unit_count` int NOT NULL COMMENT '服务的时间单位数',
+  `payment_type` int NULL DEFAULT NULL COMMENT '付款类型(先用后付, 先付后用)',
+  `service_charge` decimal(10, 2) NULL DEFAULT NULL COMMENT '服务费(路费, 材料费等)',
+  `status` int NULL DEFAULT NULL COMMENT '订单状态',
+  `rate` int NULL DEFAULT NULL COMMENT '是否已评价',
+  `is_deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已删除(0: 未删除, 1: 已删除)',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1789924361226477568 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for service
+-- ----------------------------
+DROP TABLE IF EXISTS `service`;
+CREATE TABLE `service`  (
+  `id` bigint NOT NULL COMMENT '服务id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '服务名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '服务介绍',
+  `icon_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '图标url',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for social_user
+-- ----------------------------
+DROP TABLE IF EXISTS `social_user`;
+CREATE TABLE `social_user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'auth主键',
+  `uuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '第三方唯一id 非null',
+  `source` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '认证类型 非null',
+  `access_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `refresh_token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `created_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for social_user_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `social_user_auth`;
+CREATE TABLE `social_user_auth`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` bigint NOT NULL COMMENT '系统用户id',
+  `social_user_id` bigint NOT NULL COMMENT '社会化用户id',
+  `created_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for time_table
+-- ----------------------------
+DROP TABLE IF EXISTS `time_table`;
+CREATE TABLE `time_table`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '时间表主键',
+  `order_id` bigint NOT NULL COMMENT '订单表id',
+  `start_time` datetime NOT NULL COMMENT '开始时间',
+  `end_time` datetime NOT NULL COMMENT '结束时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '订单时间表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `nickname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '昵称',
+  `gender` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '性别',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '头像',
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `default_address_id` bigint NULL DEFAULT NULL COMMENT '默认地址id',
+  `created_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `updated_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
